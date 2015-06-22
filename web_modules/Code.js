ba1c9c196258e8659d92493581fced367cd42766
Matthias Le Brun <mlbli@me.com>
2015-06-22 20:22:07 +0200

    fixed Code centering

--- web_modules/Code.js
+++ web_modules/Code.js
@@ -11,28 +11,35 @@ class Code extends Component {
   render() {
     const {code, language} = this.props
     return (
-      <pre
-        style={styles.pre}
-        dangerouslySetInnerHTML={{
-          __html:
-            highlight.highlight(language, code).value
-              .replace(/class="hljs-class"/g, "style=\"color:#1294a2\"")
-              .replace(/class="hljs-rules"/g, "style=\"color:#1294a2\"")
-              .replace(/class="hljs-value"/g, "style=\"color:#a2231e\"")
-              .replace(/class="hljs-attribute"/g, "style=\"color:#3e9a1d\"")
-              .replace(/class="hljs-comment"/g, "style=\"color:#1348a3\"")
-              .replace(/class="hljs-built_in"/g, "style=\"color:#a32661\"")
-              .replace(/class="hljs-title"/g, "style=\"color:#b35b17\"")
-              .replace(/class="hljs-string"/g, "style=\"color:#a2231e\""),
-        }} />
+      <div style={styles.container}>
+        <pre
+          style={styles.pre}
+          dangerouslySetInnerHTML={{
+            __html:
+              highlight.highlight(language, code).value
+                .replace(/class="hljs-class"/g, "style=\"color:#1294a2\"")
+                .replace(/class="hljs-rules"/g, "style=\"color:#1294a2\"")
+                .replace(/class="hljs-value"/g, "style=\"color:#a2231e\"")
+                .replace(/class="hljs-attribute"/g, "style=\"color:#3e9a1d\"")
+                .replace(/class="hljs-comment"/g, "style=\"color:#1348a3\"")
+                .replace(/class="hljs-built_in"/g, "style=\"color:#a32661\"")
+                .replace(/class="hljs-title"/g, "style=\"color:#b35b17\"")
+                .replace(/class="hljs-string"/g, "style=\"color:#a2231e\""),
+          }} />
+      </div>
     )
   }
 }
 
 const styles = {
+  container: {
+    textAlign: "center",
+  },
   pre: {
     fontSize: "2rem",
     padding: "1rem",
+    display: "inline-block",
+    textAlign: "left",
   },
 }
 
